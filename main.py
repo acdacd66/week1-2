14f03956fe51b94be692a4cdef8e7ef3218679c2
jinatra <jinatra0816@gmail.com>
2021-11-05 03:12:25 +0900

    Feature/song-CR
    ;
    ;
    ;
    ;
    ;
    ;
    ;

--- main.py
+++ main.py
@@ -11,47 +11,72 @@ user=os.getenv("user")
 pwd=os.getenv("pwd")
 
 
-class nodemodel(BaseModel):
-    name:str
+class song(BaseModel):
+    title    : str
+    musician : str
 
+class album(BaseModel):
+    title    : str
+    song     : str
+    musician : str
+
+class musician(BaseModel):
+    name : str
 
 def connection():
     driver=GraphDatabase.driver(uri=uri,auth=(user,pwd))
     return driver
 
 app=FastAPI()
-@app.post("/create_music")
-def createnode(node:nodemodel):
+@app.post("/create_song")
+def createnode(node:song):
     driver_neo4j=connection()
     session=driver_neo4j.session()
     q1="""
-    merge(n:music{name:$name}) return n.name as name
+    merge(song:song{title:$title, musician:$musician}) return song.title as title
     """
-    x={"name":node.name}
+    x={"title":node.title, "musician":node.musician}
     results=session.run(q1,x)
-    data=[{"Name":row["name"]}for row in results][0]["Name"]
-    return {"response":"node created with music name as: "+data}
+    data=[{"Title":row["title"]}for row in results][0]["Title"]
+    return {"response":"node created with song name as: "+data}
 
 @app.post("/create_album")
-def createnode(node:nodemodel):
+def createnode(node:album):
     driver_neo4j=connection()
     session=driver_neo4j.session()
     q1="""
-    merge(n:album{name:$name}) return n.name as name
+    merge(album:album{title:$title, song:$song, musician:$musician}) return album.title as title
     """
-    x={"name":node.name}
+    x={"title":node.title, "song":node.song, "musician":node.musician}
     results=session.run(q1,x)
-    data=[{"Name":row["name"]}for row in results][0]["Name"]
+    data=[{"Title":row["title"]}for row in results][0]["Title"]
     return {"response":"node created with album name as: "+data}
 
 @app.post("/create_musician")
-def createnode(node:nodemodel):
+def createnode(node:musician):
     driver_neo4j=connection()
     session=driver_neo4j.session()
     q1="""
-    merge(n:musician{name:$name}) return n.name as name
+    merge(musician:musician{name:$name}) return musician.name as name
     """
     x={"name":node.name}
     results=session.run(q1,x)
     data=[{"Name":row["name"]}for row in results][0]["Name"]
     return {"response":"node created with musician name as: "+data}
+
+
+
+@app.get("/get_song")
+def getnode(request):
+    driver_neo4j=connection()
+    session=driver_neo4j.session()
+    q1="""
+    match(song) where song.musician = "$param" return song.title as title
+    """
+    x={"param":request}
+    results=session.run(q1,x)
+    return {"response":[
+        {
+            "Title":row["title"]
+        } for row in results
+    ]}
